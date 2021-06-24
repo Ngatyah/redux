fc482c8fe2d88d474c96e9c42a1037645f084a50
ngatyah <davidngatyah@gmail.com>
2021-06-24 09:55:18 +0300

    updated Redux counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,19 @@
 import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter)
+  const toggleCounterHandler = () => { };
+  console.log(counter)
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{` ${counter}`}</div>
+      <div >
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
